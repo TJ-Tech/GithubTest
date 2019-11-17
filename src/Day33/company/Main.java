16ed96bc0b4ca45ddc4af0c4b548110d5347fed2
Tj-Tech <aktepetuncay@gmail.com>
2019-11-17 12:56:08 -0500

    second try

--- src/Day33/company/Main.java
+++ src/Day33/company/Main.java
@@ -6,5 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("Hello GitHub");
         System.out.println("Hello GitHub");
+        System.out.println("Hello GitHub");
+
     }
 }
